7ccca11488fac9adfeb6fb71e82b5f3fa6fe28af
Javier Iglesias <javiglesias18@gmail.com>
2017-06-04 20:53:54 +0200

    Starting to work on this again
    Object Oriented Changes.
    Learning how to not crash the memory.
    :()

--- text_based/text_based/lib.h
+++ text_based/text_based/lib.h
@@ -1,6 +1,23 @@
 //lib.h
 //-Igle-
 #pragma once
+#include <stdio.h>
+#include <winsock.h>//sockets for windows
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <time.h>
+//#include <cppconn/driver.h>
+//#include <cppconn/exception.h>
+//#include <cppconn/resultset.h>
+//#include <cppconn/statement.h>
+#define DEBUG 1
+#if DEBUG 
+	#define LOG(X) std::cout<<X<<std::endl;
+#else
+#define LOG(X)
+#endif
 #ifndef MAP
 	#define MAP 3
 #endif
@@ -25,34 +42,87 @@
 #ifndef ROOM4
 #define ROOM4 "Room 4 descripton."
 #endif
-
-#ifndef PRINT_ROOM_0
-#define PRINT_ROOM_0 " _______\n|\t|\n|\t|\n|\t|\n|\t|\n|_______|\n"
-#endif // !PRINT_ROOM_0
-
-
-//
-struct room
+#define STRING std::string
+typedef struct room
 {
+	int id;
 	char description[255];
-	int nort, south, east, west;
-}rooms[(MAP*MAP)];
+	room *north, *south, *east, *west;
+}room;
 
-struct object
-{
-	int state;
-	char desc1[80], desc2[80];
-	int now_room, got_it;
-}objects[2];
+class Dungeon {
+	public:
+		room *createDungeon() { 
+			room rooms[10];
+			for (int i = 0; i < 10; i++) rooms[i].id = i;
+			rooms[0].north = rooms[1].north = rooms[2].north = NULL;
+			rooms[0].west= rooms[3].west = rooms[6].west = NULL;
+			rooms[2].east = rooms[5].east = rooms[8].east = NULL;
+			rooms[6].south = rooms[7].south = rooms[8].south = NULL;
+			rooms[0].south = rooms[6].north = rooms[4].west =  &rooms[3];
+			rooms[1].south = rooms[3].east = rooms[7].north = rooms[5].west = &rooms[4];
+			return rooms;
+		};
+		~Dungeon(){};
+};
 
-struct inventory
-{
-	int obj1 = -1, obj2 = -1, obj3 = -1, obj4 = -1;
-}bag;
+class object{
+	public:
+		char desc1[80], desc2[80];
+		int now_room, got_it;
+};
 
-typedef struct rooms
-{
-	char description[140];
-	char puerta[5];
-	//north, south, east, west
-} roome;
+class Character {
+	/*
+	We are gonna have 3 main tipes of character, the mage, rogue and titan.
+	The values of heath, defense and attack depends by the main type of character.
+	*/
+	private:
+		STRING name;
+		long health, defense, attack;
+		STRING char_class = "";
+		object inventory[2];
+		room now_room;
+	public:
+		void actual_room( room now){now_room = now;};
+		int _room(){return now_room.id;};
+		void set_name(STRING _name){ name = _name; };
+		void set_class(STRING _class) { char_class =_class; };
+		STRING get_class(){return char_class;};
+		STRING get_name(){return name;};
+		void move(char direction){
+			switch (direction) {
+				case 'N':
+					std::cout << "north" << std::endl;
+					if(now_room.north != NULL)
+						now_room = *now_room.north;
+					else
+						now_room = now_room;
+					break;
+				case 'S':
+					std::cout << "sur" << std::endl;
+					if (now_room.south != NULL)
+						now_room = *now_room.south;
+					else
+						now_room = now_room;
+					break;
+				case 'E':
+					std::cout << "east" << std::endl;
+					if (now_room.east != NULL)
+						now_room = *now_room.east;
+					else
+						now_room = now_room;
+					break;
+				case 'W':
+					std::cout << "west" << std::endl;
+					if (now_room.west != NULL)
+						now_room = *now_room.west;
+					else
+						now_room = now_room;
+					break;
+				default:
+					break;
+			}
+		};
+		~Character() {};
+};
--- text_based/text_based/main.cpp
+++ text_based/text_based/main.cpp
@@ -1,85 +1,60 @@
 //main.cpp
 //Igle
-#include <stdio.h>
-#include <iostream>
-#include <math.h>
-#include <time.h>
+
 #include "lib.h"
 
+/*bool restore_game(Character ch_restore) {
+	std::ifstream ifile(".saved.bin");
+	char __name[10];
+	if (ifile) {
+		std::ifstream saved_file;
+		int file_size[2];
+		file_size[0] = saved_file.tellg();
+		saved_file.seekg(0, std::ios::end);
+		file_size[1] = saved_file.tellg();
+		std::ifstream::pos_type size = file_size[1] - file_size[0];
+		saved_file.open(".saved.bin", std::ios::binary);
+		saved_file.seekg(0, std::ios::beg);
+		saved_file.read((char *)&__name, size);
+		Character ch_restore(__name);
+		std::cout << "Nombre: " 
+			<< __name << std::endl;
+		saved_file.close();
+		return true;
+	}
+	else
+		return false;
+}
 
-
+bool save_game(Character ch_save) {
+	std::ifstream::pos_type size = sizeof(ch_save);
+	std::ofstream saved_file;
+	saved_file.open(".saved.bin", std::ios::binary);
+	saved_file.seekp(0, std::ios::beg);
+	saved_file.write((char *)&ch_save, size);
+	saved_file.close();
+	return true;
+}
+Character::Character(char *_name) {
+	strcpy_s(name,_name);
+}*/
+Character pj1;
+Dungeon mazz;
 int room_establish(int x)
 {
 	/*
-		ESTA FUNCION SE ENCARGA DE DETERMINAR DE FORMA ALEATORIA ENTRE TODAS LAS HABITACIONES
+		ESTA FUNCION SE ENCARGA DE DETERMINAR DE FORMA ALEATORIA ENTRE TODAS LAS HABITACIONES 
 		DONDE ESTARÁ EL ITEM QUE LA LLAMA
 	*/
 	srand(time(NULL)+x);
 	return rand() % (MAP*MAP);
 }
-
 void create_Map()
 {
-
 	//INICIALIZAMOS LAS HABITACIONES CON SUS DESCRIPCIONES
-	//ROOM 0
-	strcpy_s(rooms[0].description, ROOM0);
-	rooms[0].nort = -1;
-	rooms[0].south = 3;
-	rooms[0].east = 1;
-	rooms[0].west = -1;
-	//ROOM 1
-	strcpy_s(rooms[1].description, ROOM1);
-	rooms[1].nort = 0;
-	rooms[1].south = 4;
-	rooms[1].east = 2;
-	rooms[1].west = 0;
-	//ROOM 2
-	strcpy_s(rooms[2].description, ROOM2);
-	rooms[2].nort = 0;
-	rooms[2].south = 5;
-	rooms[2].east = -1;
-	rooms[2].west = 1;
-	//ROOM 3
-	strcpy_s(rooms[3].description, ROOM3);
-	rooms[3].nort = 0;
-	rooms[3].south = 6;
-	rooms[3].east = 4;
-	rooms[3].west = -1;
-	//ROOM 4
-	strcpy_s(rooms[4].description, ROOM4);
-	rooms[4].nort = 1;
-	rooms[4].south = 7;
-	rooms[4].east = 5;
-	rooms[4].west = 3;
-
-	//INICIALIZAMOS CADA UNO DE LOS ITEMS
-	std::cout << "Key in Room: "
-		<<room_establish(0)
-		<<"\n";
-	objects[0].now_room= room_establish(0);
-	strcpy_s(objects[0].desc1 ,"KEY");
-	strcpy_s(objects[0].desc2, "The KEY shine.");
-	objects[0].state = 0;
-	objects[0].got_it = 0;
-	std::cout << "Chest in Room: "
-		<<room_establish(1)
-		<< "\n";
-	objects[1].now_room = room_establish(1);
-	strcpy_s(objects[1].desc1, "CHEST");
-	strcpy_s(objects[1].desc2, "The CHEST shine.");
-	objects[1].state = 0;
-	objects[1].got_it = 0;
-	std::cout << "Lantern in Room: "
-		<< room_establish(2)
-		<< "\n";
-	objects[2].now_room = room_establish(2);
-	strcpy_s(objects[2].desc1, "LANTERN");
-	strcpy_s(objects[2].desc2, "The LANTERN is off.");
-	objects[2].state = 0;
-	objects[2].got_it = 0;
+	pj1.actual_room(mazz.createDungeon()[0]);
 }
-int find_objects(int now_Room)
+/*int find_objects(int now_Room)
 {
 	for (int i = 0; i <= 2; i++)
 	{
@@ -104,226 +79,103 @@ void free_inventory()
 	bag.obj2 = -1;
 	bag.obj3 = -1;
 	bag.obj4 = -1;
-}
+}*/
 void main(void)
 {
 	bool done = false, done2 = false;
 	int choose = -1;
 	int now_Room = 0;
 	int finder = -1;
-	while (!done)//mientras que no se pase el juego seguira dentro del bucle pero cambiando opciones
+	char __name[10] = "";
+	int char_class;
+	object inventario[5];
+	while (!done)
+	//mientras que no se pase el juego seguira dentro del bucle pero cambiando opciones
 	{
 		//mostrar info de la habitacion actual
-		std::cout << PRINT_ROOM_0;
 		std::cout << "1-Exit\n0-Create Map\n";
 		//tomar entrada de jugador
 		std::cin >> done;
 		//procesar info obtenida del jugador
 		if (!done)
 		{
-			free_inventory();
+			std::cout << "Escriba su nombre(maximo 10 caracteres):" << std::endl;
+			std::cin >> __name;
+			pj1.set_name(__name);
+			//Character ch1(__name);
+			//save_game(ch1);
+			std::cout << "Elige a tu heroe:\n"
+				<<"0-Mage\n"
+				<<"1-Titan\n"
+				<<"2-Rogue\n";
+			//tomar entrada de jugador
+			std::cin >> char_class;
+			switch (char_class) {
+				case 0:
+					pj1.set_class("Mage");
+					break;
+				case 1:
+					pj1.set_class("Titan");
+					break;
+				case 2:
+					pj1.set_class("Rogue");
+					break;
+				default:
+					break;
+			}
+			//instanciamos el mapa de la dungeon para esta partida.
 			create_Map();
+			std::cout << pj1._room() << std::endl;
 			done2 = false;
-			now_Room = 0;
 			while (!done2)
 			{
 				std::cout << "You are now in the ROOM "
-					<< now_Room;
-				finder = find_objects(now_Room);
+					<< pj1._room() << std::endl;
+				LOG(pj1.get_name());
+				LOG(pj1.get_class());
+				/*finder = find_objects(now_Room);
 				if (finder != -1)
 				{
 					std::cout << "\nIn this ROOM you have: "
 						<< objects[finder].desc1;
 				}
 				else
-					std::cout << "\nThere is no items in this ROOM";
+					std::cout << "\nThere is no items in this ROOM";*/
 				std::cout << "\nWhat do you do?";
 				std::cout << "\n1-Go North."
 					<< "\n2-Go South."
 					<< "\n3-Go East."
 					<< "\n4-Go West."
-					<< "\n5-Back."
+					<< "\n5-Inventory."
 					<< "\n6-Take."
-					<< "\n7-See Inventory."
+					<< "\n7-BACK"
 					<< "\n";
 				std::cin >> choose;
 				switch (choose)
 				{
-				case 1:
-					//Vas hacia el norte
-					if (now_Room <= 2 && now_Room>=0)
-					{
-						std::cout << "You hit with a very well built wall.\n";
-					}
-					else if (now_Room > 2)
-					{
-						now_Room = now_Room - 3;
-						std::cout << "You enter in Room"
-							<< now_Room
-							<< "\n";
-					}
-					break;
-				case 2:
-					//vas hacia el sur
-					if (now_Room >= 6)
-					{
-						std::cout << "You hit with a very well built wall.\n";
-					}
-					else if (now_Room < 6)
-					{
-						now_Room = now_Room + 3;
-						std::cout << "You enter in Room"
-							<< now_Room
-							<< "\n";
-					}
-					break;
-				case 3:
-					//vas hacia el este
-					if (now_Room < 8)
-					{
-						if (now_Room == 2)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else if (now_Room == 5)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else if (now_Room == 8)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else
-						{
-							now_Room = now_Room + 1;
-							std::cout << "You enter in Room"
-								<< now_Room
-								<< "\n";
-						}
-
-					}
-					else
-						std::cout << "You hit with a very well built wall.\n";
-					break;
-				case 4:
-					//vas hacia el oeste
-					if (now_Room > 0)
-					{
-						if (now_Room == 0)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else if (now_Room == 3)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else if (now_Room == 6)
-						{
-							std::cout << "You hit with a very well built wall.\n";
-							break;
-						}
-						else
-						{
-							now_Room = now_Room - 1;
-							std::cout << "You enter in Room"
-								<< now_Room
-								<< "\n";
-						}
-
-					}
-					else
-						std::cout << "You hit with a very well built wall.\n";
-					break;
-				case 5:
-					done2 = true;
-					break;
-				case 6:
-					if (finder != -1)
-					{
-						std::cout << "You take: "
-							<< objects[finder].desc1
-							<< "\n";
-						clean_floor(finder);
-						if (bag.obj1 != -1)
-						{
-							bag.obj1 = finder;
-						}
-						else if (bag.obj2 != -1)
-						{
-							bag.obj2 = finder;
-						}
-						else if (bag.obj3 != -1)
-						{
-							bag.obj3 = finder;
-						}
-						else if (bag.obj4 != -1)
-						{
-							bag.obj4 = finder;
-						}
-						else
-						{
-							std::cout << "The inventory is full."
-								<< "\n";
-							break;
-						}
-						finder = -1;
-
-					}
-					else
-					{
-						std::cout << "There is nothing to take in this ROOM."
-							<< "\n";
+					case 1:
+						pj1.move('N');
 						break;
-					}
-				case 7:
-					if (bag.obj1 == -1)
-					{
-						std::cout << "The inventory is empty."
-							<< "\n";
+					case 2:
+						pj1.move('S');
 						break;
-					}
-					else if (bag.obj2 == -1)
-					{
-						std::cout << bag.obj1
-							<< "\n";
+					case 3:
+						pj1.move('E');
 						break;
-					}
-					else if (bag.obj3 == -1)
-					{
-						std::cout << bag.obj1
-							<< "\n"
-							<< bag.obj2
-							<< "\n";
+					case 4:
+						pj1.move('W');
 						break;
-					}
-					else if (bag.obj4 == -1)
-					{
-						std::cout << bag.obj1
-							<< "\n"
-							<< bag.obj2
-							<< "\n"
-							<< bag.obj3
-							<< "\n";
+					case 5:
+						LOG("Under construction!!!");
 						break;
-					}
-					else
-					{
-						std::cout << bag.obj1
-							<< "\n"
-							<< bag.obj2
-							<< "\n"
-							<< bag.obj3
-							<< "\n"
-							<< bag.obj4
-							<< "\n";
+					case 6:
+						LOG("Under construction!!!");
 						break;
-					}
-					break;
+					case 7:
+						done2 = true;
+						break;
+					default:
+						break;
 				}
 			}
 		}
@@ -334,5 +186,4 @@ void main(void)
 			system("pause");
 		}
 	}
-	//system("CLS");//limpia la pantalla de la terminal
 }
